0bbbd6f33666a1a568f51145f0a277c59f0078d9
Ankit Das <96786190+PiKa919@users.noreply.github.com>
2022-12-30 20:11:07 +0530

    Add files via upload

---
+++ TODO/function.py
@@ -0,0 +1,15 @@
+FILEPATH = 'todos.txt'
+
+def get_todos(filepath = FILEPATH):
+    """Read a text file and return the list of to-dos items"""
+
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_args,filepath = FILEPATH):
+    """Write a to-do items list in the text file"""
+
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_args)
